e79c939ac6c3382fc48d7842b0d27a3e3014f3f3
Senvor <adarshkushwahaop@gmail.com>
2026-09-25 15:09:50 +0530

    Add initial HTML and JavaScript files for particle simulation

---
+++ particle.js
@@ -0,0 +1,21 @@
+class Particle {
+  constructor(x, y, mass) {
+    //Setup particles
+  }
+
+  draw() {
+    //Draw particles
+  }
+
+  applyForce(force) {
+    //Apply force to particle
+  }
+
+  physics(particle) {
+    //Use particles
+  }
+
+  update() {
+    //Update particles
+  }
+}
